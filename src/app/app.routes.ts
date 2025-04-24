b7df2b912896c978c87e264c9d248afab1ed6b18
moustafamohasseb00 <moustafamohaseb00@hotmail.com>
2025-04-24 02:59:37 +0200

    patientlist ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { NotFoundComponent } from './core/features/not-found/not-found.component';
 import { LoginComponent } from './core/features/auth/login/login.component';
 import { AppointmentComponent } from './core/features/appointments/appointment/appointment.component';
+import { PatientListComponent } from './core/features/patients/patient-list/patient-list.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {path:'appointments', component: AppointmentComponent},
+  {path:'patients',component:PatientListComponent},
   { path: '**', component: NotFoundComponent },
 ];
