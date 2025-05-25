8077e9fd5338c392e447b575e40fb37f67137a1a
MostafSayed1 <mostafasayed7410@gmail.com>
2025-05-25 05:55:23 +0300

    moving assets folder inside public

--- src/app/core/layouts/master-layout/master-layout.component.ts
+++ src/app/core/layouts/master-layout/master-layout.component.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-master-layout',
-  imports: [SidebarComponent,FooterComponent,RouterOutlet],
+  imports: [SidebarComponent,RouterOutlet],
   templateUrl: './master-layout.component.html',
   styleUrl: './master-layout.component.css'
 })
