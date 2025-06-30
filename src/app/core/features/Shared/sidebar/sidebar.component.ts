4b7dd4164ddc7d29a7ff26b7f5f053c0ca243fb0
MostafSayed1 <mostafasayed7410@gmail.com>
2025-07-01 01:30:55 +0300

    last update

--- src/app/core/features/Shared/sidebar/sidebar.component.ts
+++ src/app/core/features/Shared/sidebar/sidebar.component.ts
@@ -1,15 +1,18 @@
 // sidebar.component.ts
 import { Component } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
+  imports: [CommonModule,RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
   // Track collapsed states
-  constructor(private authService:AuthService){}
+  constructor(protected authService:AuthService){}
   menuStates = {
     mainMenu: true,
     managementMenu: true,
@@ -22,6 +25,6 @@ export class SidebarComponent {
   // }
 
   logout() {
-    this.authService.logout() ; 
+    this.authService.logout() ;
   }
 }
